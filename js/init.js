45c7931c61afeccc4dfd782d03422d7a0b425f36
martin29anzaldo <matinutn10@gmail.com>
2021-07-25 16:21:28 -0500

    cambio

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBvqk0gH6mFFvHNRNlHfKaWwLPX62Uy_GI",
+    authDomain: "bfalumnos41.firebaseapp.com",
+    projectId: "bfalumnos41",
+    storageBucket: "bfalumnos41.appspot.com",
+    messagingSenderId: "977302937922",
+    appId: "1:977302937922:web:0694536fb9f3abaec0a8db",
+    measurementId: "G-4T0PFM1G87"
+});
