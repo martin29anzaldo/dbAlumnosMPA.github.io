cd685f1458b10b83329a52ef664c82262b6578c2
martin29anzaldo <87622691+martin29anzaldo@users.noreply.github.com>
2021-07-25 16:48:28 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyBvqk0gH6mFFvHNRNlHfKaWwLPX62Uy_GI",
-    authDomain: "bfalumnos41.firebaseapp.com",
-    projectId: "bfalumnos41",
-    storageBucket: "bfalumnos41.appspot.com",
-    messagingSenderId: "977302937922",
-    appId: "1:977302937922:web:0694536fb9f3abaec0a8db",
-    measurementId: "G-4T0PFM1G87"
+    apiKey: "AIzaSyD346wFfzyAkWsqi50qXHS1PDscRJJM7HE",
+    authDomain: "bdfalumnosmpa.firebaseapp.com",
+    projectId: "bdfalumnosmpa",
+    storageBucket: "bdfalumnosmpa.appspot.com",
+    messagingSenderId: "739572912244",
+    appId: "1:739572912244:web:6ac2e108f593314109c121",
+    measurementId: "G-QJMWY0N3W6"
 });
